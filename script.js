2242fcea625b292e19fc41d5d889f12d298c5f50
Ayodejioladimeji <layobright11@gmail.com>
2021-04-18 04:40:28 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// Sidebar Toggle Codes;
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+var sidebarCloseIcon = document.getElementById("sidebarIcon");
+
+function toggleSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar_responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar_responsive");
+    sidebarOpen = false;
+  }
+}
+
+
+// The section of the delete popup
+// When the user clicks on div, open the popup
+function myFunction() {
+  var popup = document.getElementById("pop");
+  popup.classList.toggle("show");
+}
